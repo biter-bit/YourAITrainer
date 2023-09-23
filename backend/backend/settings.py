e57108c1b03c39a87a31ba8dc02809cf5570b50b
sslava <ricar@mail.ru>
2023-09-23 12:52:32 +0300

    add change

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -93,23 +93,23 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-#DATABASES = {
-#   'default': {
-#       'ENGINE': 'django.db.backends.sqlite3',
-#       'NAME': BASE_DIR / 'db.sqlite3',
-#   }
-#}
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        "NAME": os.environ.get("POSTGRES_DB"),
-        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
-        "USER": os.environ.get("POSTGRES_USER"),
-        "HOST": os.environ.get("DATABASE_HOST"),
-        "PORT": os.environ.get("POSTGRES_PORT"),
-     }
+   'default': {
+       'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': BASE_DIR / 'db.sqlite3',
+   }
 }
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        "NAME": os.environ.get("POSTGRES_DB"),
+#        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+#        "USER": os.environ.get("POSTGRES_USER"),
+#        "HOST": os.environ.get("DATABASE_HOST"),
+#        "PORT": os.environ.get("POSTGRES_PORT"),
+#     }
+#}
 
 
 # Password validation
