830aab820f81bf022f296ebaac840866887a5b94
michael <kurashevmichael@gmail.com>
2023-09-25 19:23:10 +0300

    update front

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,8 +6,11 @@ import Articles from "./components/Articles"
 import Offer from "./components/Offer";
 import Burger from "./components/Burger";
 import 'bootstrap/dist/css/bootstrap.css';
-// import axios from "axios";
+import axios from "axios";
 
+
+const link_api_auth = 'http://localhost:8000/api/auth/jwt/create/'
+const link_api_register = 'http://localhost:8000/api/auth/register/'
 class App extends React.Component {
     constructor(props) {
         super(props)
@@ -16,27 +19,44 @@ class App extends React.Component {
             modelActiveReg: false,
             authorized: false,
             burger_active: false,
+            response: [],
+            error: ""
         }
         this.inputClickAuth = this.inputClickAuth.bind(this)
         this.inputClickReg = this.inputClickReg.bind(this)
         this.authorizedAuth = this.authorizedAuth.bind(this)
         this.funcBurgerActive = this.funcBurgerActive.bind(this)
+        this.checkLocalStorage = this.checkLocalStorage.bind(this)
+        this.funcAuth = this.funcAuth.bind(this)
+        this.funcRegistered = this.funcRegistered.bind(this)
     }
     render() {
         return (
             <div>
-                <Burger burger={this.state.burger_active} setBurger={this.funcBurgerActive}/>
+                <Burger burger={this.state.burger_active} setBurger={this.funcBurgerActive} setAuth={this.funcAuth}/>
                 <div className="container">
                     <Menu setActive={this.inputClickAuth} auth={this.state.authorized} setBurger={this.funcBurgerActive}/>
                       <div className="container_body">
                         <Offer setActive={this.inputClickReg}/>
                         <Articles />
                       </div>
-                      <Register active={this.state.modelActiveReg} setActive={this.inputClickReg} />
-                      <Auth active={this.state.modelActiveAuth} setActive={this.inputClickAuth} setAuth={this.authorizedAuth}/>
+                      <Register active={this.state.modelActiveReg} setActive={this.inputClickReg} error={this.state.error} setRegister={this.funcRegistered}/>
+                      <Auth active={this.state.modelActiveAuth} setActive={this.inputClickAuth} setAuth={this.authorizedAuth} error={this.state.error}/>
                 </div>
             </div>
         )
+    }
+    componentDidMount() {
+        this.checkLocalStorage()
+    }
+
+    checkLocalStorage() {
+        const access = localStorage.getItem("access")
+        const refresh = localStorage.getItem("refresh")
+        if (access && refresh){
+            const data = { access, refresh }
+            this.setState({ response: data, authorized: true })
+        }
     }
     funcBurgerActive() {
         console.log(this.state.burger_active)
@@ -47,15 +67,71 @@ class App extends React.Component {
             this.setState({ burger_active: true })
         }
     }
-    authorizedAuth(event) {
-        event.preventDefault()
+    funcAuth() {
         if (this.state.authorized) {
             this.setState({ authorized: false })
-            this.inputClickAuth()
         }
         else {
             this.setState({ authorized: true })
-            this.inputClickAuth()
+        }
+    }
+    async funcRegistered(event) {
+        event.preventDefault()
+        const form = event.target
+        const formData = new FormData(form)
+        try {
+            const response = await axios.post(link_api_register, {
+                username: formData.get("username"),
+                password: formData.get("password"),
+                password_confirmation: formData.get("password_confirmation"),
+                email: formData.get("email")
+            })
+            this.inputClickReg()
+        } catch (error) {
+            if (error.response && error.response.status === 400) {
+                this.setState({
+                    error: "Неверные данные",
+                });
+            } else {
+                this.setState({
+                    error: "Произошла ошибка при входе. Пожалуйста, попробуйте еще раз.",
+                });
+            }
+        }
+    }
+    async authorizedAuth(event) {
+        event.preventDefault()
+        const form = event.target
+        const formData = new FormData(form)
+        try {
+            const response = await axios.post(link_api_auth, {
+                username: formData.get("username"),
+                password: formData.get("password"),
+            })
+            localStorage.setItem("access", response.data.access);
+            localStorage.setItem("refresh", response.data.refresh)
+
+            const data = localStorage.getItem("access")
+
+            if (data == null) {
+                this.setState({ authorized: false })
+                this.inputClickAuth()
+            }
+
+            else {
+                this.setState({ authorized: true })
+                this.inputClickAuth()
+            }
+        } catch (error) {
+            if (error.response && error.response.status === 401) {
+                this.setState({
+                    error: "Неправильный логин или пароль",
+                });
+            } else {
+                this.setState({
+                    error: "Произошла ошибка при входе. Пожалуйста, попробуйте еще раз.",
+                });
+            }
         }
     }
     inputClickAuth() {
