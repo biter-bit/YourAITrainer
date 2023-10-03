968b2c3e2024eeac8537d577eaeb02f4b848faf3
Liuba <kundasl1@gmail.com>
2023-10-03 16:38:17 +0300

    change env and db

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -91,23 +91,24 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'movies',
-        'USER': 'movies',
-        'PASSWORD': '123456',
-        'HOST': 'db',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'D:\\IT\\projects\\YourAITrainer\\backend\\db.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'movies',
+#        'USER': 'movies',
+#        'PASSWORD': '123456',
+#        'HOST': 'db',
+#        'PORT': '5432',
+#    }
+#}
 
 
 # Password validation
@@ -210,6 +211,7 @@ else:
     CORS_ALLOWED_ORIGINS = [
         "http://localhost",
         "http://127.0.0.1",
+        "http://localhost:3000",
     ]
 
 #djoser
