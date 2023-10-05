580c1e049a778a4c4c06957d3629b49d5e3f39c7
Liuba <kundasl1@gmail.com>
2023-10-06 01:29:54 +0300

    settings вернула обратно

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -91,24 +91,24 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'D:\\IT\\projects\\YourAITrainer\\backend\\db.sqlite3',
-        #'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 #DATABASES = {
 #    'default': {
-#        'ENGINE': 'django.db.backends.postgresql',
-#        'NAME': 'movies',
-#        'USER': 'movies',
-#        'PASSWORD': '123456',
-#        'HOST': 'db',
-#        'PORT': '5432',
+#        'ENGINE': 'django.db.backends.sqlite3',
+#
+#        #'NAME': BASE_DIR / 'db.sqlite3',
 #    }
 #}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'movies',
+        'USER': 'movies',
+        'PASSWORD': '123456',
+        'HOST': 'db',
+        'PORT': '5432',
+    }
+}
 
 
 # Password validation
