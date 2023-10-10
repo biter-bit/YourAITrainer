9028420b70ddc027d3a4c5164cf467bb123a9aae
AndreiGosovich <andrei_gosovich@mail.ru>
2023-10-10 22:00:51 +0300

    Change loading articles from vc.ru Обновлена логика загрузки статей. Scrapy заменён на BeautifulSoup. Создание записей в базе по обновлённой модели. Добавлена загрузка краткого описания и картинок. В main.css добавлен стиль для img. Для больших экранов ширина картинок не была установлена и большие расползались.

--- backend/services/get_articles.py
+++ backend/services/get_articles.py
@@ -1,7 +1,10 @@
 import httpx
 import asyncio
 import feedparser
-from scrapy.selector import Selector
+from bs4 import BeautifulSoup
+import os.path
+
+from article.models import Article
 
 
 async def site_parser(httpx_client, link_parse):
@@ -13,12 +16,12 @@ async def site_parser(httpx_client, link_parse):
         print(f'Ошибка подключения к {link_parse}')
         exit(1)
 
-    selector = Selector(text=response.text)
+    soup = BeautifulSoup(response.text, "html.parser")
     article_text = ''
-    for row in selector.xpath('//div[@class="content content--full "]')[0].xpath('//div[@class="l-island-a"]')[1:-2]:
-        raw_text = row.xpath('*//text()').extract()
+    for row in soup.select(".content--full")[0].find_all("div", class_='l-island-a')[1:-2]:
+        raw_text = row.find_next('p').text
         if raw_text:
-            article_text += f'{raw_text[0]}\n'
+            article_text += f'{raw_text}\n'
         else:
             article_text += f'\n'
     return article_text
@@ -38,16 +41,32 @@ async def rss_parser(httpx_client, rss_link):
         result = []
 
         for entry in feed.entries[::-1]:
-            # summary = entry['description']
+            summary = entry['description']
             title = entry['title']
             source = entry['link']
             article_text = await site_parser(httpx_client, source)
-            # news_text = f'Заголовок: {title}\nИсточник: {source}\nТекст статьи:\n{article_text}'
+            file = ''
+
+            for link in entry['links']:
+                if link['rel'] == 'enclosure' and link['type'].find('image') >= 0:
+                    file_name = f'./media/images/{entry["guid"]}.{link["type"].split("/")[-1]}'
+
+                    if not os.path.isfile(file_name):
+                        with open(file_name, 'wb') as download_file:
+                            url = link['href']
+                            with httpx.stream("GET", url) as response:
+                                for chunk in response.iter_bytes():
+                                    download_file.write(chunk)
+                            print(download_file.name)
+                    file = '/'.join(file_name.split('/')[-2:])
+                    continue
 
             result.append({
                 'title': title,
                 'source': source,
                 'article_text': article_text,
+                'short_description': summary,
+                'file': file,
             })
 
         return result
@@ -57,7 +76,26 @@ def start_articles_import():
     rss_link = 'https://vc.ru/rss/tag/fitness'
     httpx_client = httpx.AsyncClient()
 
-    return asyncio.run(rss_parser(httpx_client, rss_link))
+    imports = asyncio.run(rss_parser(httpx_client, rss_link))
+    count = 0
+
+    if not imports:
+        return False, 0
+
+    for article in imports:
+        if not len(Article.objects.filter(title=article['title'])):
+            news_article = Article(
+                title=article['title'],
+                content=article['article_text'],
+                short_description=article['short_description'],
+                source=article['source'],
+                file=article['file'],
+                published=True,
+            )
+            news_article.save()
+            count += 1
+
+    return True, count
 
 
 if __name__ == "__main__":
