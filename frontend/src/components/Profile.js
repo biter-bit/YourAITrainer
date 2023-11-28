4e42232dfe3a8fa96f4cf66b0c251783943e6bdb
michael <kurashevmichael@gmail.com>
2023-11-28 11:30:32 +0300

    add ip

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import axios from "axios";
 
-const link_api_save_profile = 'http://91.200.84.202:8000/api/users/profile/update';
+const link_api_save_profile = 'http://91.200.84.202:80/api/users/profile/update';
 
 class WindowProfile extends React.Component {
 
